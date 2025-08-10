752f7b8c7dc700e8cd7608a12373aa3d79a1ccad
Chinmay R Nadugeri <chinmaynadugeri6@gmail.com>
2025-08-11 02:21:05 +0530

    Update DBConnection.java

--- Online Reservation System/OnlineReservationSystem/src/DBConnection.java
+++ Online Reservation System/OnlineReservationSystem/src/DBConnection.java
@@ -2,10 +2,11 @@ import java.sql.*;
 
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/reservationdb";
-    private static final String USER = "root";
-    private static final String PASS = "Chinnu@01";
+    private static final String USER = "root"; // Enter your MySQL user ID here
+    private static final String PASS = ""; // Enter your MySQL password here
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
+
